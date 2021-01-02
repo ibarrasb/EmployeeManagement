182977ec8b3ffdc90a603b5a3ccb27efa83b0646
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-01-02 13:24:48 -0600

    Updated employees by deprtment

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const sql = require("mysql");
+const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "Employee_Management_db"
+  });
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    promptUser();
+  });
+
+function promptUser(){
 inquirer
     .prompt([
         {
@@ -20,10 +34,12 @@ inquirer
 
         switch(ans.res){
             case "View All Employees":
-
+            showEmployees();
+            promptUser();
             break;
 
             case "View All Employees by Dept":
+            byDept();
 
             break;
 
@@ -43,3 +59,63 @@ inquirer
 
 
     })
+
+}
+
+function showEmployees(){
+    connection.query("SELECT * FROM employee", function(err, res) {
+        if (err) throw err;
+
+        for(var i = 0; i < res.length; i++){
+        
+            console.log(`${res[i].first_name} ${res[i].last_name}`);
+            console.log('========================================');
+        }
+    
+    })
+
+
+}
+
+function byDept(){
+
+    inquirer.prompt([
+        {
+            name: "dept",
+            type: "list",
+            message: "Which department would you like to view? ",
+            choices: 
+            ["IT",
+             "Production",
+             "Engineering",
+             "Accounting",
+             "Sales"
+            ]
+        }
+    ]).then(function(ans){
+
+        switch(ans.dept){
+
+            case 'IT':
+
+            break;
+
+            case 'Production':
+
+            break;
+
+            case 'Engineering':
+                
+            break;
+
+            case 'Accounting':
+                
+            break;
+
+            case 'Sales':
+                
+            break;
+        }
+    })
+
+}
